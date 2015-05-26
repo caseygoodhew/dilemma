a9461a89ef1c621d308ffc1a055517a2d548b457
caseygoodhew <goodhewc@gmail.com>
2015-05-26 07:24:31 +0100

    System.Logging baseline

---
+++ code/Dilemma.Logging/Registration.cs
@@ -0,0 +1,19 @@
+﻿using Disposable.Common.ServiceLocator;
+using NLog;
+
+namespace Dilemma.Logging
+{
+    public static class Registration
+    {
+        /// <summary>
+        /// Registers business services with the <see cref="ILocator"/> <see cref="IRegistrar"/>.
+        /// </summary>
+        /// <param name="registrar">The <see cref="IRegistrar"/> to use.</param>
+        public static void Register(IRegistrar registrar)
+        {
+            registrar.Register<ILogger>(() => new LoggerAdapter(LogManager.GetCurrentClassLogger()));
+
+            Locator.Current.Instance<ILogger>().Info("Logger starting");
+        }
+    }
+}
