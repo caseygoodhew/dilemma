3138d0f8f9de02413f45add827d7c215441b4c69
caseygoodhew <goodhewc@gmail.com>
2015-06-17 09:10:00 +0100

    Remove development access

--- code/Dilemma.Web/Controllers/ModerationController.cs
+++ code/Dilemma.Web/Controllers/ModerationController.cs
@@ -11,7 +11,7 @@ using Disposable.Common.ServiceLocator;
 
 namespace Dilemma.Web.Controllers
 {
-    [AllowSystemEnvironment(SystemEnvironment.Development)]
+    //[AllowSystemEnvironment(SystemEnvironment.Development)]
     [AllowUserRole(UserRole.Moderator)]
     public class ModerationController : DilemmaBaseController
     {
