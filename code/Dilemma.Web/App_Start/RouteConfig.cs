5013c504a194e33c2c387d78db794a0c148c9a53
caseygoodhew <goodhewc@gmail.com>
2015-08-08 07:55:13 +0100

    Reverse Proxy

--- code/Dilemma.Web/App_Start/RouteConfig.cs
+++ code/Dilemma.Web/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace Dilemma.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("blog/{*pathInfo}");
 
             routes.MapMvcAttributeRoutes();
 
